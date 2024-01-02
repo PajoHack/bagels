dd8450e08a16ce6d4e3db13cc104add6c037701e
Patrick Hackman <pajohack@gmail.com>
2024-01-02 17:58:56 +0000

    Correcting typo in intro

--- bagels.py
+++ bagels.py
@@ -13,17 +13,17 @@ MAX_GUESSES = 10
 
 def main():
     print('''Bagels, a deductive logic game.
-By Pajo
 
-I am thinking of a {}-digit number with no repeated digits.
-Try to guess what it is. Here are some clues:
-When I say:    That means:
-Pico           One digit is correct but in the wrong position.
-Fermi          One digit is correct and in the right position.
-Bagels         No digit is correct.
 
-For example, if the secret number was 248 and your guess was 843, the
-clueswould be Fermi Pico.'''.format(NUM_DIGITS)) 
+    I am thinking of a {}-digit number with no repeated digits.
+    Try to guess what it is. Here are some clues:
+    When I say:    That means:
+    Pico           One digit is correct but in the wrong position.
+    Fermi          One digit is correct and in the right position.
+    Bagels         No digit is correct.
+
+    For example, if the secret number was 248 and your guess was 843, the
+    clues would be Fermi Pico.'''.format(NUM_DIGITS)) 
     
     while True: # Main game loop.
         # This stores the secret number the player needs to guess:
@@ -31,12 +31,12 @@ clueswould be Fermi Pico.'''.format(NUM_DIGITS))
         print('I have thought up a number.')
         print(' You have {} guesses to get it.'.format(MAX_GUESSES))
         
-        numGusses = 1
-        while numGusses <= MAX_GUESSES:
+        numGuesses = 1
+        while numGuesses <= MAX_GUESSES:
             guess = ''
             # Keep looping until they enter a valid guess:
             while len(guess) != NUM_DIGITS or not guess.isdecimal():
-                print('Guess #{}: '.format(numGusses))
+                print('Guess #{}: '.format(numGuesses))
                 guess = input('> ')
                 
                 clues = getClues(guess, secretNum)
